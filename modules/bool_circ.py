5e6ae85330afae7dae02e6488bb1bf1d150c00d8
Ivan <ivkharkov04@gmail.com>
2025-02-21 17:17:11 +0100

    did is_well_formed for bool_circ(has to be tested)

--- modules/bool_circ.py
+++ modules/bool_circ.py
@@ -37,4 +37,46 @@ class bool_circ(open_digraph):
         """
         return not self.is_acyclic()
     
+    def is_well_formed(self):
+        """
+        Tests if a graph is well formed:
+            - The graph is acyclic
+            -Degree constraints:
+                - '' (copy) - indegree = 1
+                - '&' (and), '|' (or), '^' (xor) - outdegree = 1
+                - '~' (not) - indegree = 1, outdegree = 1
+                - '1', '0' - indegree = 0
+
+        Returns:
+            True - if the graph is well formed
+            False - otherwise
+        """
+
+        if not self.is_acyclic():
+            return False
+
+        for node in self.get_nodes():
+            label = node.get_label()
+            in_d = node.indegree()
+            out_d = node.outdegree()
+            node_d = node.degree()
+
+            if label == '':
+                if in_d != 1:
+                    return False
+            elif label in {'&', '|','^'}:
+                if out_d != 1:
+                    return False
+            elif label == '~':
+                if out_d != 1 or in_d != 1:
+                    return False
+            elif label in  {'1', '0'}:
+                if out_d != 0:
+                    return False
+            else: 
+                return False
+        return True
+
+
+    
 
