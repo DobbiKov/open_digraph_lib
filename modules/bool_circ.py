aebabe26de0b8f7fe0bbba9e723651172eddda06
Yehor Korotenko <dobbikov@gmail.com>
2025-02-21 21:45:50 +0100

    fixed constructor of bool_circ

--- modules/bool_circ.py
+++ modules/bool_circ.py
@@ -3,9 +3,13 @@ from modules.open_digraph import node, open_digraph
 
 class bool_circ(open_digraph):
     def __init__(self, g: open_digraph):
+        if not g.is_well_formed():
+            g = open_digraph.empty()
+            
         super().__init__(g.inputs, g.outputs, list(g.nodes.values()))
         if not self.is_well_formed():
-            self = self.empty()
+            g = open_digraph.empty()
+            super().__init__(g.inputs, g.outputs, list(g.nodes.values()))
 
     
     def is_well_formed(self):
@@ -22,8 +26,6 @@ class bool_circ(open_digraph):
             True - if the graph is well formed
             False - otherwise
         """
-        if not super().is_well_formed():
-            return False
 
         if not self.is_acyclic():
             return False
