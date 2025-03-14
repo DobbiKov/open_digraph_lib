745e9b6780f8ac7556028ca4f72dd7e94d4ad5ce
Yehor Korotenko <dobbikov@gmail.com>
2025-03-14 16:35:42 +0100

    added report and presentation 2

--- presentation2.py
+++ presentation2.py
@@ -30,12 +30,31 @@ def print_matrix(mat: list[list[int]]):
 
 # particular graph
 # test1
-
+print("Test 1, press Enter to continue")
+input()
 mat_graph1 = random_oriented_int_matrix(10, 5)
 print_matrix(mat_graph1)
+
+# test2
+print("Test 2, press Enter to continue")
+input()
 graph1 = open_digraph.from_matrix(mat_graph1)
+print(graph1.is_well_formed())
+print(graph1.get_nodes_ids())
+
+# test3
+print("Test 3, press Enter to continue")
+input()
 graph1.display()
 
+# test4
+print("Test 4, press Enter to continue")
+input()
 graph1.add_input_node(1)
-graph1.add_output_node(5)
+print(graph1.is_well_formed())
+print(graph1.get_nodes_ids())
+# graph1.add_output_node(5)
+# test5
+print("Test 5, press Enter to continue")
+input()
 graph1.display("after_add_input")
